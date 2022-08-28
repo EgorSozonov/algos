448d35950ca98c99bfbe8ff395a91f093b984072
EgorSozonov <Egor.Sozonov.09@gmail.com>
2022-08-28 18:45:46 +0300

    .

--- source/MaxSumOfRectangle/MaxSumOfRectangle.kt
+++ source/MaxSumOfRectangle/MaxSumOfRectangle.kt
@@ -50,13 +50,15 @@ fun solution(matrix: Array<IntArray>, requiredSum: Int): Int {
                     if (i > 0) {
                         subSum -= prefixSums[i - 1][l]
                         if (j > 0) {
-                            subSum -= prefixSums[k][j - 1]
+                            subSum += prefixSums[i -1][j - 1]
                         }
                     }
                     if (j > 0) {
                         subSum -= prefixSums[k][j - 1]
                     }
-                    if (subSum == requiredSum) return requiredSum
+                    if (subSum == requiredSum) {
+                        return requiredSum
+                    } // [1, 1] [11, 2]
                     if (subSum < requiredSum && subSum > bestSum) {
                         bestSum = subSum
                     }
@@ -69,8 +71,26 @@ fun solution(matrix: Array<IntArray>, requiredSum: Int): Int {
 }
 
 fun runner() {
-    val inputs = arrayOf(Triple(arrayOf( intArrayOf(1, 0, 1), intArrayOf(0, -2, 3) ), 2, 2),
-                                                     Triple(arrayOf( intArrayOf(2, 2, -1)), 3, 3),
+    val inputs = arrayOf(
+        Triple(arrayOf(intArrayOf(27,5,-20,-9,1,26,1,12,7,-4,8,7,-1,5,8),
+            intArrayOf(16,28,8,3,16,28,-10,-7,-5,-13,7,9,20,-9,26),
+            intArrayOf(24,-14,20,23,25,-16,-15,8,8,-6,-14,-6,12,-19,-13),
+            intArrayOf(28,13,-17,20,-3,-18,12,5,1,25,25,-14,22,17,12),
+            intArrayOf(7,29,-12,5,-5,26,-5,10,-5,24,-9,-19,20,0,18),
+            intArrayOf(-7,-11,-8,12,19,18,-15,17,7,-1,-11,-10,-1,25,17),
+            intArrayOf(-3,-20,-20,-7,14,-12,22,1,-9,11,14,-16,-5,-12,14),
+            intArrayOf(-20,-4,-17,3,3,-18,22,-13,-1,16,-11,29,17,-2,22),
+            intArrayOf(23,-15,24,26,28,-13,10,18,-6,29,27,-19,-19,-8,0),
+            intArrayOf(5,9,23,11,-4,-20,18,29,-6,-4,-11,21,-6,24,12),
+            intArrayOf(13,16,0,-20,22,21,26,-3,15,14,26,17,19,20,-5),
+            intArrayOf(15,1,22,-6,1,-9,0,21,12,27,5,8,8,18,-1),
+            intArrayOf(15,29,13,6,-11,7,-6,27,22,18,22,-3,-9,20,14),
+            intArrayOf(26,-6,12,-10,0,26,10,1,11,-10,-16,-18,29,8,-8),
+            intArrayOf(-19,14,15,18,-10,24,-9,-7,-19,-14,23,23,17,-5,6)
+        ), -100, -101),
+        Triple(arrayOf( intArrayOf(1, 0, 1), intArrayOf(0, -2, 3) ), 2, 2),
+        Triple(arrayOf( intArrayOf(2, 2, -1)), 3, 3),
+
     )
     var correctResults = 0
     for (input in inputs) {
